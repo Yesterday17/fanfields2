5a494cfb554c971fa16730ccaeb57666c3e268f0
Yesterday17 <mmf@mmf.moe>
2023-11-13 13:11:04 +0800

    Update iitc_plugin_fanfields2.meta.js

--- iitc_plugin_fanfields2.meta.js
+++ iitc_plugin_fanfields2.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name            IITC plugin: Fan Fields 2
-// @id              fanfields@heistergand
+// @name            IITC plugin: Fan Fields 2 [Yesterday17]
+// @id              fanfields@Yesterday17
 // @author          Heistergand
 // @category        Layer
 // @version         2.5.3.20230919
@@ -8,10 +8,10 @@
 // @match           https://intel.ingress.com/*
 // @include         https://intel.ingress.com/*
 // @grant           none
-// @downloadURL     https://github.com/Heistergand/fanfields2/raw/master/iitc_plugin_fanfields2.user.js
-// @updateURL       https://github.com/Heistergand/fanfields2/raw/master/iitc_plugin_fanfields2.meta.js
-// @icon            https://raw.githubusercontent.com/Heistergand/fanfields2/master/fanfields2-32.png
-// @icon64          https://raw.githubusercontent.com/Heistergand/fanfields2/master/fanfields2-64.png
-// @supportURL      https://github.com/Heistergand/fanfields2/issues
-// @namespace       https://github.com/Heistergand/fanfields2
+// @downloadURL     https://github.com/Yesterday17/fanfields2/raw/master/iitc_plugin_fanfields2.user.js
+// @updateURL       https://github.com/Yesterday17/fanfields2/raw/master/iitc_plugin_fanfields2.meta.js
+// @icon            https://raw.githubusercontent.com/Yesterday17/fanfields2/master/fanfields2-32.png
+// @icon64          https://raw.githubusercontent.com/Yesterday17/fanfields2/master/fanfields2-64.png
+// @supportURL      https://github.com/Yesterday17/fanfields2/issues
+// @namespace       https://github.com/Yesterday17/fanfields2
 // ==/UserScript==
